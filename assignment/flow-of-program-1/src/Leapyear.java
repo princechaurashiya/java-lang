0ce51a7ef541e4601fad86f57e13db067973a3ec
cprincej <princechaurashiya163@gmail.com>
2024-02-19 12:27:53 +0530

    --

---
+++ assignment/flow-of-program-1/src/Leapyear.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Leapyear {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter Year");
+        int year = in.nextInt();
+
+        if (year % 4 == 0 && year % 100 !=0){
+            System.out.println("Given number is Leap Year");
+        } else if
+            (year % 400 == 0) {
+            System.out.println("Given Year is a leap Year");
+        }
+        else{
+            System.out.println("Giver year is not a leap year");
+        }
+    }
+}
